22ae0b5d8d5eb90a9f708e0601b26f868a7d25b9
David Antonio Ty <22-1-00262@vsu.edu.ph>
2024-11-24 22:28:09 +0800

    added orders

--- pages/success.vue
+++ pages/success.vue
@@ -1,13 +1,9 @@
 <template>
-  <MainLayout>
+  <AdminLayout>
     <div id="SuccessPage" class="mt-4 max-w-[1200px] mx-auto px-2 min-h-[50vh]">
       <div class="bg-white w-full p-6 min-h-[150px]">
         <div class="flex items-center text-xl">
-          <Icon
-            name="clarity:success-standard-line"
-            color="#5FCB04"
-            size="35"
-          />
+          <Icon name="clarity:success-standard-line" color="#5FCB04" size="35" />
           <span class="pl-4">Order Successful</span>
         </div>
         <p class="text-sm pl-[50px]">
@@ -15,11 +11,11 @@
         </p>
       </div>
     </div>
-  </MainLayout>
+  </AdminLayout>
 </template>
 
 <script setup>
-import MainLayout from "~/layouts/MainLayout.vue";
+import AdminLayout from "~/layouts/AdminLayout.vue";
 import { useUserStore } from "~/stores/user";
 const userStore = useUserStore();
 const user = useSupabaseUser();
